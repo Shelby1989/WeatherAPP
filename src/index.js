58a8bbcf4d67e477eef439beaa61c37cdad94777
Shelby1989 <115674650+Shelby1989@users.noreply.github.com>
2022-11-06 01:45:57 -0400

    made icons change with weather

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ h2.innerHTML = `${day} ${hours}:${minutes}`;
 
 
 function displayWeatherCondition(response) {
+  let iconElement = document.querySelector("#weathericon");
+
   document.querySelector("#city").innerHTML = response.data.name;
  
   document.querySelector("#temp").innerHTML = Math.round(
@@ -27,6 +29,9 @@ function displayWeatherCondition(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed)
+
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchLocation(position) {
   let apiKey = "880be6f76144feab4c58ddbc72edd9b8";
@@ -60,3 +65,8 @@ currentLocationButton.addEventListener("click", getCurrentLocation);
 
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", submitSearch);
+
+let city = "Hershey";
+let apiKey = "880be6f76144feab4c58ddbc72edd9b8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeatherCondition);
